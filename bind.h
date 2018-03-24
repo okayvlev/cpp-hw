48dda9329e29564b4d3d375a844b0b609a3040b2
yakovlev-i <ilyuhaya@gmail.com>
2018-03-24 04:10:08 +0300

    Initial commit

---
+++ bind.h
@@ -0,0 +1,156 @@
+#include <tuple>
+
+template <int N>
+struct placeholder
+{ };
+
+constexpr placeholder<1> _1;
+constexpr placeholder<2> _2;
+constexpr placeholder<3> _3;
+
+template <bool once, typename F, typename... As>
+struct bind_t;
+
+template <typename A, typename T_0, typename... Ts>
+struct type_count
+{
+	static constexpr size_t value = (type_count<A, Ts...>::value)
+								  +  std::is_same<A, T_0>::value;
+};
+
+template <typename A, typename T>
+struct type_count<A, T>
+{
+	static constexpr size_t value = std::is_same<A, T>::value;
+};
+
+template <typename A, bool once, typename F, typename... As, typename... Ts>
+struct type_count<A, bind_t<once, F, As...>, Ts...>
+{
+	static constexpr size_t value = type_count<A, As...>::value
+								  + type_count<A, Ts...>::value;
+};
+
+template <typename T, bool>
+struct ref_check
+{ };
+
+template <typename T>
+struct ref_check<T, true>
+{
+	using type = T&&;
+};
+
+template <typename T>
+struct ref_check<T, false>
+{
+	using type = T&;
+};
+
+template <typename T, int i, typename... Ts>
+using unique_guard_t =
+	typename ref_check<T, type_count<const placeholder<i + 1>&, Ts...>::value == 1>::type;
+
+template <bool once, typename A>
+struct G
+{
+    G(A&& a)
+        : a(std::forward<A>(a))
+    { }
+
+    template <typename... Bs>
+    decltype(auto) operator()(Bs&&...)
+    {
+        return static_cast<typename ref_check<std::decay_t<A>, once>::type>(a);
+    }
+
+    typename std::decay_t<A> a;
+};
+
+template <bool once>
+struct G<once, const placeholder<1>&>
+{
+    G(const placeholder<1>&)
+    { }
+
+    template <typename B1, typename... Bs>
+    decltype(auto) operator()(B1&& b1, Bs&&...)
+    {
+        return std::forward<B1>(b1);
+    }
+};
+
+template <bool once, int N>
+struct G<once, const placeholder<N>&>
+{
+    G(const placeholder<N>&)
+    { }
+
+    template <typename B, typename... Bs>
+    decltype(auto) operator()(B&&, Bs&&... bs)
+    {
+        G<once, const placeholder<N - 1>&> next((placeholder<N - 1>()));
+        return next(std::forward<Bs>(bs)...);
+    }
+};
+
+template <bool once, bool d_once, typename F, typename... As>
+struct G<once, bind_t<d_once, F, As...>>
+{
+    G(bind_t<d_once, F, As...>&& f)
+        : f(std::forward<bind_t<d_once, F, As...>>(f))
+    { }
+
+    template <typename... Bs>
+    decltype(auto) operator()(Bs&&... bs)
+    {
+        return f(std::forward<Bs>(bs)...);
+    }
+
+    bind_t<d_once, F, As...> f;
+};
+
+template <bool once, typename F, typename... As>
+struct bind_t
+{
+    bind_t(F&& f, As&&... as)
+        : f(std::forward<F>(f))
+        , gs(std::forward<As>(as)...)
+    { }
+
+    template <typename... Bs>
+    decltype(auto) operator()(Bs&&... bs)
+    {
+        return call(
+			std::make_integer_sequence<int, sizeof...(As)>(),
+			std::make_integer_sequence<int, sizeof...(Bs)>(),
+			std::forward<Bs>(bs)...
+			);
+    }
+
+private:
+    template <int... ks, int... is, typename... Bs>
+    decltype(auto) call(
+		std::integer_sequence<int, ks...>,
+		std::integer_sequence<int, is...>, // iterating and checking for placeholder uniqueness
+		Bs&&... bs
+		)
+    {
+        return f(std::get<ks>(gs)(unique_guard_t<Bs, is, As...>(bs)...)...);
+    }
+
+	std::decay_t<F> f;
+    std::tuple<G<once, As>...> gs;
+};
+
+template <typename F, typename... As>
+decltype(auto) bind(F&& f, As&&... as)
+{
+    return bind_t<false, F, As...>(std::forward<F>(f), std::forward<As>(as)...);
+}
+
+template <typename F, typename... As>
+decltype(auto) call_once_bind(F&& f, As&&... as)
+{
+    return bind_t<true , F, As...>(std::forward<F>(f), std::forward<As>(as)...);
+}
---
+++ unit_test.cpp
@@ -0,0 +1,227 @@
+#include "gtest/gtest.h"
+#include "bind.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+struct my_struct {
+	my_struct() {}
+	my_struct(my_struct const&) {
+		logger += "c";
+	}
+	my_struct(my_struct&&) {
+		logger += "m";
+	}
+	static std::string logger;
+};
+std::string my_struct::logger;
+
+void f(my_struct) {
+
+}
+
+void hw() {
+
+}
+
+TEST(testBind, helloWorld) {
+	auto w = bind(hw);
+	w();
+}
+
+TEST(testBind, moveFixedArgument) {
+	my_struct::logger = "";
+
+	auto w = bind(f, my_struct());
+	EXPECT_EQ(my_struct::logger, "m");
+	w();
+	EXPECT_EQ(my_struct::logger, "mc");
+}
+
+TEST(testBind, copyFixedArgument) {
+	my_struct::logger = "";
+	my_struct x;
+
+	auto w = bind(f, x);
+	EXPECT_EQ(my_struct::logger, "c");
+	w();
+	EXPECT_EQ(my_struct::logger, "cc");
+}
+
+TEST(testBind, movePlaceholder) {
+	my_struct::logger = "";
+	auto w = bind(f, _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(my_struct());
+	EXPECT_EQ(my_struct::logger, "m");
+}
+
+TEST(testBind, copyPlaceholder) {
+	my_struct::logger = "";
+	my_struct x;
+	auto w = bind(f, _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(x);
+	EXPECT_EQ(my_struct::logger, "c");
+}
+
+void g(my_struct, my_struct) {
+
+}
+
+TEST(testBind, copyUnuniquePlaceholder) {
+	my_struct::logger = "";
+	auto w = bind(g, _1, _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(my_struct());
+	EXPECT_EQ(my_struct::logger, "cc");
+}
+
+void gg(my_struct, my_struct, my_struct) {
+
+}
+
+TEST(testBind, uniqueAndUnuniquePlaceholdersTogether) {
+	my_struct::logger = "";
+	auto w = bind(gg, _1, _2, _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(my_struct(), my_struct(), my_struct());
+	EXPECT_TRUE(my_struct::logger == "mcc" || my_struct::logger == "cmc" || my_struct::logger == "ccm");
+}
+
+struct simpleFo {
+	void operator()() {}
+	simpleFo() = default;
+	simpleFo(simpleFo const&) {
+		logger += "c";
+	}
+	simpleFo(simpleFo&&) {
+		logger += "m";
+	}
+	static std::string logger;
+};
+std::string simpleFo::logger;
+
+TEST(testBind, moveSimpleFunctionalObject) {
+	simpleFo::logger = "";
+	auto w = bind(simpleFo());
+	EXPECT_EQ(simpleFo::logger, "m");
+	w();
+	EXPECT_EQ(simpleFo::logger, "m");
+}
+
+TEST(testBind, copySimpleFunctionalObject) {
+	simpleFo::logger = "";
+	simpleFo x;
+	auto w = bind(x);
+	EXPECT_EQ(simpleFo::logger, "c");
+	w();
+	EXPECT_EQ(simpleFo::logger, "c");
+}
+
+bool comparator(int a, int b) {
+	return bind([](int a, int b) -> bool {return a > b; }, _1, _2)(a, b);
+}
+
+TEST(testBind, correctness) {
+	std::vector<int> v(1000);
+	for (size_t i = 0; i < v.size(); i++) {
+		v[i] = rand();
+	}
+	std::sort(v.begin(), v.end(), comparator);
+	for (size_t i = 1; i < v.size(); i++) {
+		EXPECT_TRUE(v[i - 1] >= v[i]);
+	}
+}
+
+void bar(my_struct const) {
+
+}
+
+TEST(testBind, constFixedArgument) {
+	my_struct::logger = "";
+	auto w = bind(bar, my_struct());
+	EXPECT_EQ(my_struct::logger, "m");
+	w();
+	EXPECT_EQ(my_struct::logger, "mc");
+}
+
+TEST(testBind, constPlaceholderArgument) {
+	my_struct::logger = "";
+	auto w = bind(bar, _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(my_struct());
+	EXPECT_EQ(my_struct::logger, "m");
+}
+
+void foo(int a[]) {
+	a[0] = 10;
+	a[1] = 20;
+}
+
+int a[10];
+
+TEST(testBind, arrayFixedArgument) {
+	a[0] = a[1] = 0;
+	auto w = bind(foo, a);
+	w();
+	EXPECT_EQ(a[0], 10);
+	EXPECT_EQ(a[1], 20);
+}
+
+TEST(testBind, arrayPlaceholderArgument) {
+	a[0] = a[1] = 0;
+	auto w = bind(foo, _1);
+	w(a);
+	EXPECT_EQ(a[0], 10);
+	EXPECT_EQ(a[1], 20);
+}
+
+my_struct& rec(my_struct& a, my_struct& b) {
+	return a;
+}
+
+TEST(testBind, recBind) {
+	my_struct::logger = "";
+	my_struct x, y, z;
+	auto w = bind(rec, bind(rec, x, y), z);
+	EXPECT_EQ(my_struct::logger.length(), size_t(5));
+	my_struct::logger = "";
+	w();
+	EXPECT_EQ(my_struct::logger, "");
+}
+
+TEST(testBind, recBind2) {
+	my_struct::logger = "";
+	my_struct x, y;
+	auto w = bind(rec, bind(rec, _1, _2), _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(x, y);
+	EXPECT_EQ(my_struct::logger, "");
+}
+
+TEST(testBind, recBind3) {
+	my_struct::logger = "";
+	my_struct x, y;
+	auto w = call_once_bind(rec, bind(rec, _1, _2), _1);
+	EXPECT_EQ(my_struct::logger, "");
+	w(x, y);
+	EXPECT_EQ(my_struct::logger, "");
+}
+
+TEST(testCallOnceBind, moveFixedRvalueArgument) {
+	my_struct::logger = "";
+	auto w = call_once_bind(f, my_struct());
+	EXPECT_EQ(my_struct::logger, "m");
+	w();
+	EXPECT_EQ(my_struct::logger, "mm");
+}
+
+TEST(testCallOnceBind, moveFixedLvalueArgument) {
+	my_struct::logger = "";
+	my_struct x;
+	auto w = call_once_bind(f, x);
+	EXPECT_EQ(my_struct::logger, "c");
+	w();
+	EXPECT_EQ(my_struct::logger, "cm");
+}
